0e143f5407baed65ac24346b059ac6059a2e7cc8
arkerrig <arkerrig@gmail.com>
2020-01-15 08:56:54 -0800

    updated application_controller for login redir

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+def require_login
+  unless logged_in?
+    flash[:error] = "You must be logged in to access"
+    redirect_to articles_path
+  end
+end
+
 end
